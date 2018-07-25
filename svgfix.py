a11535457c4f84e9ec08f41ba2d7fc4117da4130
Kevin Marks <kevin.marks.ext@bbc.co.uk>
2018-07-25 02:51:32 +0100

    Fix deeply nested evil fractals

--- svgfix.py
+++ svgfix.py
@@ -2,6 +2,9 @@ import html5lib
 import html5lib.serializer
 import html5lib.treewalkers
 import openanything
+import logging
+
+maxuse = 36
 
 def svgfix(src):
     # Parse SRC as HTML.
@@ -9,13 +12,22 @@ def svgfix(src):
     parser = html5lib.html5parser.HTMLParser(tree = tree_builder)
     dom = parser.parse(src)
     hadScript = False
+    overUse = False
     svg = dom.getElementsByTagName('svg')[0]
+    usecount = 0;
+    for g in svg.getElementsByTagName('g'):
+        for u in g.getElementsByTagName('use'):
+            usecount= usecount+1
+            if usecount> maxuse:
+                g.removeChild(u)
+                overUse = True
+                logging.info("usecount: '%s' " %(usecount))
     for s in svg.getElementsByTagName('script'):
         svg.removeChild(s)
         hadScript = True
     tree_walker = html5lib.treewalkers.getTreeWalker('dom')
     html_serializer = html5lib.serializer.htmlserializer.HTMLSerializer(quote_attr_values=True)
-    return u''.join(html_serializer.serialize(tree_walker(svg))),hadScript
+    return u''.join(html_serializer.serialize(tree_walker(svg))),hadScript or overUse
 
 def urlfix(url):
     return svgfix(openanything.fetch(url).get('data',''))
